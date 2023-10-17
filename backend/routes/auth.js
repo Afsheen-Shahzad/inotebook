1c01834e44a9458d74de1658b0b4ecef5cd57c5d
Afsheen <sheen0555@gmail.com>
2023-10-17 23:12:08 +0500

    fifth commit - Creating thunderclient collections

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -6,30 +6,37 @@ const { body, validationResult } = require('express-validator');
 const router  = express.Router();
 
 
-//create a user using : POST '/api/auth' endpoint. It doesn't required authentication 
+//create a user using : POST '/api/auth/createuser' endpoint. It doesn't required authentication 
 
 //router.get('/',(req,res)=>{
-router.post('/',[
+router.post('/createuser',[
     body('name','Please enter correct name').isLength({ min: 5 }),
     body('username','Please enter correct username').isLength({min: 5}),
     body('password','Password length should be atleast 5').isLength({ min: 5 }),
     body('email','Enter valid email').isEmail()
-],(req,res)=>{
+],async (req,res)=>{
+  //if there are errors, return baad request and errors
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     } 
-
-    User.create({
+//check whether the user with this email exists already
+try {
+    let user = await User.findOne({email : req.body.email})
+    if (user){
+      return res.status(400).json({ error: "sorry a user with this email is already exists" });
+    }
+    user= await User.create({
         name: req.body.name,
         username: req.body.username,
         password: req.body.password,
         email: req.body.email
-      }).then(user => res.json(user)).catch(err =>
-        {
-            console.log(err)
-            res.json({error : "Please enter a unique value for email/username", message : err.message})
-    })
+      })
+    res.json({user})
+}catch (error) {
+  console.error(error.message);
+  res.status(500).send("Some error occured")
+}
 })
 
 module.exports = router;
