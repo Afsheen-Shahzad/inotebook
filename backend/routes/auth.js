c37e3663ef510df509322a40b9541ebd215e60b7
Afsheen <sheen0555@gmail.com>
2023-10-20 16:11:48 +0500

    Sixth commit - Hashing password & JSON WEB TOKEN

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -3,8 +3,12 @@ const User = require('../models/User');
 
 const { body, validationResult } = require('express-validator');
 
+var bcrypt = require('bcryptjs');
+
 const router  = express.Router();
 
+var jwt = require('jsonwebtoken');
+const JWT_secret = "harryisagoodboy";
 
 //create a user using : POST '/api/auth/createuser' endpoint. It doesn't required authentication 
 
@@ -14,6 +18,7 @@ router.post('/createuser',[
     body('username','Please enter correct username').isLength({min: 5}),
     body('password','Password length should be atleast 5').isLength({ min: 5 }),
     body('email','Enter valid email').isEmail()
+  /////https://express-validator.github.io/docs/6.15.0/
 ],async (req,res)=>{
   //if there are errors, return baad request and errors
     const errors = validationResult(req);
@@ -26,13 +31,26 @@ try {
     if (user){
       return res.status(400).json({ error: "sorry a user with this email is already exists" });
     }
+    //v use await bcoz it returens a promise
+    //https://www.npmjs.com/package/bcryptjs
+    var salt = await bcrypt.genSaltSync(10);
+    const secPassword = await bcrypt.hashSync(req.body.password, salt);
+
     user= await User.create({
         name: req.body.name,
         username: req.body.username,
-        password: req.body.password,
+        password: secPassword,
         email: req.body.email
       })
-    res.json({user})
+      const data ={
+        user: {
+          id: user.id
+        }
+      }
+      const authToken = jwt.sign(data, JWT_secret);
+      console.log(authToken)
+      res.json({authToken})
+      //res.json(user);
 }catch (error) {
   console.error(error.message);
   res.status(500).send("Some error occured")
@@ -41,4 +59,11 @@ try {
 
 module.exports = router;
 
-///https://express-validator.github.io/docs/6.15.0/
+
+
+// Concepts
+
+// https://express-validator.github.io/docs/6.15.0/
+// https://www.npmjs.com/package/bcryptjs
+// https://www.npmjs.com/package/jsonwebtoken
+// https://jwt.io/
