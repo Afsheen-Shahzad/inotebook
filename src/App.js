2517cc0d66c7faad877afe2ff536e136cd35fbf9
Afsheen <sheen0555@gmail.com>
2023-10-21 21:17:37 +0500

    11th commit-Navbar and Routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import './App.css';
+import Navbar from './components/Navbar';
+import {Home} from './components/Home'
+import About from './components/About'
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <h1>This is my react app Inotebook</h1>
-    </div>
+    <>
+      <BrowserRouter>
+          <Navbar />
+          <Routes>
+            <Route exact path="/home" element={<Home />} />
+            <Route exact path="/about" element={<About />} />
+          </Routes>
+        </BrowserRouter>
+      
+    </>
   );
 }
 
 export default App;
+
+// https://reactrouter.com/en/main/start/overview
